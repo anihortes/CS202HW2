2bd90c402c5b0384dcf08b5a2bf58e8f62d47766
Adam Owen <ahowenjr@alaska.edu>
2021-02-09 22:11:10 -0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <list>
+using std::list;
+struct Value{
+
+};
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
